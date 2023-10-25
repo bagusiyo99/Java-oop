3ac9510cecf1c73f70c0d06b8f6a58c3cc010870
Bagus <bagusdp.1811010081@mail.darmajaya.ac.id>
2023-10-25 21:04:21 +0700

    test

--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,16 @@
  class Person {
 
+    String name;
+    String address;
+    final String country = "indonesia";
+
+    void sayHello (String paramName) {
+        System.out.println("Hello " + paramName + ", My Name is " + name);
+    }
+
+
+
+
 }
+
+
--- src/PersonApp.java
+++ src/PersonApp.java
@@ -1,15 +1,30 @@
 public class PersonApp {
     public static void main(String[] args) {
 
+
+
         var person1 = new Person();
+        person1.name = "joko";
+        person1.address = "balam";
+
+
+        System.out.println(person1.name);
+        System.out.println(person1.address);
+
+        person1.sayHello("bagus");
+
+
         Person person2 = new Person();
 
         Person person3;
         person3 = new Person();
+        person3.name = "joko";
+        person3.sayHello("bagus");
 
-        System.out.println(person1);
-        System.out.println(person2);
-        System.out.println(person3);
+//        System.out.println(person1);
+//        System.out.println(person2);
+//        System.out.println(person3);
+
 
 
     }
