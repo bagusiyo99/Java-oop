3b4889dd4adb3fe45e42661803ca9a243b09f61d
Bagus <bagusdp.1811010081@mail.darmajaya.ac.id>
2023-10-27 22:05:14 +0700

    test

--- src/Person.java
+++ src/Person.java
@@ -6,9 +6,9 @@
     final String country = "indonesia";
 
      //Constructor Overloading
-    Person (String paramName, String paramAddress){
-        name = paramName;
-        address = paramAddress;
+    Person (String name, String address){
+        this.name = name;
+        this.address = address;
     }
 
      Person (String paramName){
@@ -21,8 +21,8 @@
      }
 
 
-    void sayHello (String paramName) {
-        System.out.println("Hello " + paramName + ", My Name is " + name);
+    void sayHello (String name) {
+        System.out.println("Hello " + name + ", My Name is " + this.name);
     }
 
 
